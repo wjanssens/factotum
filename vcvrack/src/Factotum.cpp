2130b3e7755cabe049e8885bbb3a8ed0caf83f6a
Warren Janssens <warren.janssens@gmail.com>
2020-11-24 14:18:52 -0700

    fixing component coords

--- vcvrack/src/Factotum.cpp
+++ vcvrack/src/Factotum.cpp
@@ -95,33 +95,33 @@ struct TutorialWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(6.160, 11.769)), module, Factotum::FRQ_PARAM));
-		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(36.640, 11.769)), module, Factotum::SEL_PARAM));
-		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(6.160, 42.249)), module, Factotum::OFF_PARAM));
-		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(36.640, 42.249)), module, Factotum::SCL_PARAM));
-		addParam(createParamCentered<TL1105>(mm2px(Vec(23.800, 84.760)), module, Factotum::MODE_PARAM));
+		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(10.2, 25.4)), module, Factotum::FRQ_PARAM));
+		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(40.6, 25.4)), module, Factotum::SEL_PARAM));
+		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(10.2, 48.3)), module, Factotum::OFF_PARAM));
+		addParam(createParamCentered<Rogan3PSWhite>(mm2px(Vec(40.6, 48.3)), module, Factotum::SCL_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(25.4, 81.3)), module, Factotum::MODE_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.160, 112.840)), module, Factotum::VOCT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(21.400, 112.840)), module, Factotum::CLK_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(112.840, 112.840)), module, Factotum::SCL_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.2, 111.7)), module, Factotum::VOCT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.4, 111.7)), module, Factotum::CLK_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(40.6, 111.7)), module, Factotum::SCL_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(6.160, 67.120)), module, Factotum::A_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.400, 67.120)), module, Factotum::B_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(36.640, 67.120)), module, Factotum::C_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(36.640, 82.360)), module, Factotum::D_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(36.640, 97.600)), module, Factotum::E_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.400, 97.600)), module, Factotum::F_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(6.160, 97.600)), module, Factotum::G_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(6.160, 82.360)), module, Factotum::H_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.2, 66.1)), module, Factotum::A_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(25.4, 66.1)), module, Factotum::B_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.6, 66.1)), module, Factotum::C_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.6, 81.3)), module, Factotum::D_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.6, 96.6)), module, Factotum::E_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(25.4, 96.6)), module, Factotum::F_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.2, 96.6)), module, Factotum::G_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.2, 81.3)), module, Factotum::H_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(18.820, 23.900)), module, Tutorial::A_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(23.900, 21.890)), module, Tutorial::B_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(28.980, 23.900)), module, Tutorial::C_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(30.990, 28.980)), module, Tutorial::D_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(28.980, 34.060)), module, Tutorial::E_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(23.900, 36.070)), module, Tutorial::F_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(18.820, 34.060)), module, Tutorial::G_LIGHT));
-		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(16.820, 28.980)), module, Tutorial::H_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(20.3, 31.7)), module, Tutorial::A_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(25.4, 29.7)), module, Tutorial::B_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(30.5, 31.7)), module, Tutorial::C_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(32.5, 36.8)), module, Tutorial::D_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(30.5, 41.9)), module, Tutorial::E_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(25.4, 40.0)), module, Tutorial::F_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(20.3, 41.9)), module, Tutorial::G_LIGHT));
+		addChild(createLightCentered<MediumLight<RedGreenLight>>(mm2px(Vec(18.3, 36.8)), module, Tutorial::H_LIGHT));
 	}
 };
 
